d5aab7983f4db04b2e659ee7495d2d3628ce5310
maryrose2024 <marybanawa16@gmail.com>
2024-05-03 14:56:21 +0800

    added buttons to navigate into pages

--- calendar.php
+++ calendar.php
@@ -9,62 +9,22 @@
 ?>
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
-
+    <?php include('header_nav.php');?>
     <!-- Main Content -->
     <div id="content">
-
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-            <!-- Sidebar Toggle (Topbar) -->
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                <i class="fa fa-bars"></i>
-            </button>
-
-            <!-- Topbar Search Display Only-->
-            <input class="form-control" type="text" placeholder="                                                                                  
-        Sto. Niño Parish Record-Keeping Information System" aria-label="Disabled input example" disabled readonly>
-
-            <!-- Topbar Navbar -->
-            <ul class="navbar-nav ml-auto">
-
-
-                <!-- Nav Item - User Information -->
-                <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-800 large">Mary Rose</span>
-                        <img class="img-profile rounded-circle mr-2" src="img/logo.png">
-                    </a>
-                    <!-- Dropdown - User Information -->
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="profile.php">
-                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-500"></i>
-                            Profile
-                        </a>
-
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-500"></i>
-                            Logout
-                        </a>
-                    </div>
-                </li>
-
-            </ul>
-
-        </nav>
-
         <div class="col-12 col-xl-12">
             <div class="col mt-4">
                 <h1 class="mb-2 text-uppercase fw-bolder">Event Calendar</h1>
                 <hr>
-                <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal"
-                    data-bs-target="#addScheduleModal">
-                    Add Schedule
-                </button>
-
+                <div class="d-flex gap-2 align-items-center">
+                    <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal"
+                        data-bs-target="#addScheduleModal">
+                        Add Schedule
+                    </button>
+                    <button type="button" onclick="window.location.href='schedules.php'" class="btn btn-secondary mb-3">
+                        Schedule List
+                    </button>
+                </div>
 
                 </ul>
             </div>
--- schedules.php
+++ schedules.php
@@ -7,68 +7,21 @@
 
 <div id="content-wrapper" class="d-flex flex-column">
     <div id="content">
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                <i class="fa fa-bars"></i>
-            </button>
-
-            <input class="form-control" type="text" placeholder="Sto. Niño Parish Record-Keeping Information System"
-                aria-label="Disabled input example" disabled readonly>
-
-            <ul class="navbar-nav ml-auto">
-                <li>
-                    <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                        aria-labelledby="searchDropdown">
-                        <form class="form-inline mr-auto w-100 navbar-search">
-                            <div class="input-group">
-                                <input class="form-control" type="text" id="searchInput" placeholder="Search for...">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" type="button">
-                                        <i class="fas fa-search fa-sm"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
-
-                <div class="topbar-divider d-none d-sm-block"></div>
-
-                <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-800 large">
-                            <?php echo $_SESSION['full_name'];?>
-                        </span>
-                        <img class="img-profile rounded-circle mr-2" src="img/logo.png">
-                    </a>
-                    <!-- Dropdown - User Information -->
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="profile.php">
-                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-500"></i>
-                            Profile
-                        </a>
-
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-500"></i>
-                            Logout
-                        </a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-
+        <?php include('header_nav.php');?>
         <div class="col-12 col-xl-12">
             <div class="col mt-4">
                 <h1 class="mb-2 text-uppercase fw-bolder">Event Calendar</h1>
                 <hr>
-                <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal"
-                    data-bs-target="#addScheduleModal">
-                    Add Schedule
-                </button>
+                <div class="d-flex gap-2 align-items-center">
+                    <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal"
+                        data-bs-target="#addScheduleModal">
+                        Add Schedule
+                    </button>
+                    <button type="button" onclick="window.location.href='calendar.php'" class="btn btn-secondary mb-3">
+                        Calendar
+                    </button>
+                </div>
+
                 <div class="row">
                     <h3 class="fw-bolder">Schedule List</h3>
                     <?php
